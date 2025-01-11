9469b066aa8f7a8ac8f5c41da452af2a9ebb91ca
AttoBlaze <jonaserbouandersen@gmail.com>
2025-01-11 18:46:48 +0100

    unit tests for explain and help

--- UnitTests/Commands/SingleInput.cs
+++ UnitTests/Commands/SingleInput.cs
@@ -9,4 +9,19 @@ public class SingleInputTests {
         Program.ExecuteAll(Commands.ListObjects.listables.Keys.Select(n => "list("+n+")"));
         Assert.Pass();
     }
+
+    [Test]
+    public void Help() {
+        Program.START();
+        Program.Execute("help()");
+        Assert.Pass();
+    }
+
+    [Test]
+    public void Explain() {
+        Program.START();
+        Program.Execute("explain(help)");
+        Program.Execute("explain(AlwaysWrite)");
+        Assert.Pass();
+    }
 }
--- UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
+++ UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff1770b189e891fa67a69bdc4b777c992f6e00a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8bbec7e1d400824f7d31e3b26ee9ebed29e6441")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
